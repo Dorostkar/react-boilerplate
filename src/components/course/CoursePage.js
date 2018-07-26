680297c5e322155d279f9685e9e26ac8482d098f
Sina <sina.md88@gmail.com>
2018-07-26 16:47:41 +0100

    babel presets added for using arrow function

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class CoursePage extends Component {
-    
-    render() { 
-        return ( 
-            <div>
-                <h1>Courses</h1>
-            </div>
-         )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+        course: { title: "" }
+    };
 }
- 
+
+  onTitleChange=(event)=>{
+    const course=this.state.course;
+    course.title=event.target.value;
+    this.setState({course:course})
+  };
+  onClickSave=(event)=>{
+      alert(`Saving ${this.state.course.title}`)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" onClick={this.onClickSave} value="Save" />
+      </div>
+    );
+  }
+}
+
 export default CoursePage;
