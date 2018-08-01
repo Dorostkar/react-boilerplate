4a99c2f0ba9cca856d70d2e6c92d14d1e9694b63
Sina <sina.md88@gmail.com>
2018-08-01 15:08:05 +0100

    redux setuped

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-import App from './components/App'
+import {Provider} from 'react-redux';
+import configureStore from './store/configureStore'
 import Routes from './routes';
-
 
 import "./styles/styles.css"; //webpack can import css files too!.
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
+const store=configureStore();
 
 
 render(
-    <BrowserRouter><Routes/></BrowserRouter>,
+    <Provider store={store}>
+    <BrowserRouter><Routes/></BrowserRouter>
+    </Provider>,
     document.getElementById('app')
 );
